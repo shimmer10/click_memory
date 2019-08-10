38992f26c219d4aa3c2789abf074edc360361d42
Jennifer Grace <jennifergrace@Jennifers-MacBook-Pro.local>
2019-08-10 10:01:54 -0400

    Updates

---
+++ precache-manifest.e6805475ae9a8f8cd3b8a46f9e424a8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44435acd876a88250bad5f79a3f865ec",
+    "url": "/click_memory/index.html"
+  },
+  {
+    "revision": "8528837664409098899e",
+    "url": "/click_memory/static/css/main.948484bd.chunk.css"
+  },
+  {
+    "revision": "5c8f55b8660b597406d1",
+    "url": "/click_memory/static/js/2.7e7d9017.chunk.js"
+  },
+  {
+    "revision": "8528837664409098899e",
+    "url": "/click_memory/static/js/main.ad521ba3.chunk.js"
+  },
+  {
+    "revision": "69649bf3edf95c61f066",
+    "url": "/click_memory/static/js/runtime~main.4b4cbf2d.js"
+  },
+  {
+    "revision": "282883b0c275a859fbad7a3fcee8f368",
+    "url": "/click_memory/static/media/background.282883b0.jpg"
+  }
+]);
